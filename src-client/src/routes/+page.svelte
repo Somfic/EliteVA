1126e44a7e6a799bdd7066171686d6a2926fca03
Lucas <29230467+Somfic@users.noreply.github.com>
2024-04-14 15:21:04 +0200

    Added a download prompt before installing

--- src-client/src/routes/+page.svelte
+++ src-client/src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import Logo from '$lib/Logo.svelte';
-
+	import SvelteMarkdown from 'svelte-markdown';
 	import { invoke } from '@tauri-apps/api/tauri';
 	import { listen } from '@tauri-apps/api/event';
 	import { onMount } from 'svelte';
@@ -20,9 +20,26 @@
 		error_message: string;
 	}
 
+	interface NewVersion {
+		title: string;
+		version: string;
+		changelog: string;
+		url: string;
+	}
+
 	onMount(() => {
 		progress = 'fetching';
 		text = 'Starting updater ... ';
+
+		listen('new_version', (event) => {
+			let message = event.payload as NewVersion;
+			title = message.title;
+			version = message.version;
+			changelog = message.changelog;
+			url = message.url;
+
+			invoke('show_window');
+		});
 
 		listen('message', (event) => {
 			let message = event.payload as Message;
@@ -37,14 +54,22 @@
 		});
 
 		listen('error', (event) => {
+			isPrompting = false;
 			let message = event.payload as Error;
 			progress = 'none';
 			text = message.message;
 			error_message = message.error_message;
 		});
 
-		invoke('update_eliteva');
+		invoke('get_new_version');
 	});
+
+	let isPrompting = true;
+
+	let title: string;
+	let version: string;
+	let changelog: string;
+	let url: string;
 
 	let progress: number | 'fetching' | 'none' = 'none';
 	let text: string = '';
@@ -54,23 +79,68 @@
 		// Copy error message to clipboard
 		navigator.clipboard.writeText(error_message);
 	}
+
+	function updateNow() {
+		isPrompting = false;
+		invoke('update_now');
+	}
+
+	function updateLater() {
+		isPrompting = false;
+		invoke('update_later');
+	}
+
+	function skipUpdate() {
+		isPrompting = false;
+		invoke('skip_update');
+	}
 </script>
 
-<main>
-	<Logo />
-	<h3>VoiceAttack plugin for Elite: Dangerous</h3>
-	<div class="progress" class:none={progress == 'none'}>
-		{#if progress === 'none'}
-			<div class="progress-bar" style="width: 0%"></div>
-		{:else if progress === 'fetching'}
-			<div class="progress-bar fetching" style="width: 100%"></div>
-		{:else}
-			<div class="progress-bar number" style="width: {progress}%"></div>
-		{/if}
-	</div>
-	<h2>{text}</h2>
-	{#if error_message}
-		<button on:click={() => copy()}>Copy error code</button>
+<main class:prompt={isPrompting}>
+	{#if isPrompting}
+		<div class="header">
+			<h3>A new EliteVA update is available!</h3>
+			<h1>{title}</h1>
+			<h2 class="pill">EliteVA {version}</h2>
+		</div>
+		<div class="content">
+			<p><b>Changelog</b></p>
+			<SvelteMarkdown source={changelog} />
+			<!-- <a href={url} target="_blank">See GitHub release</a> -->
+		</div>
+		<div class="footer">
+			<div class="buttons">
+				<button class="main" on:click={() => updateNow()}>Update now</button>
+				<button on:click={() => updateLater()}>Remind me later</button>
+				<!-- <button on:click={() => skipUpdate()}>Skip this version</button> -->
+			</div>
+		</div>
+	{:else}
+		<div class="header">
+			<Logo />
+			<h3>VoiceAttack plugin for Elite: Dangerous</h3>
+		</div>
+		<div class="content">
+			<div class="progress" class:none={progress == 'none'}>
+				{#if progress === 'none'}
+					<div class="progress-bar" style="width: 0%"></div>
+				{:else if progress === 'fetching'}
+					<div class="progress-bar fetching" style="width: 100%"></div>
+				{:else}
+					<div class="progress-bar number" style="width: {progress}%"></div>
+				{/if}
+			</div>
+		</div>
+		<div class="footer">
+			<h3>{text}</h3>
+			<div class="buttons">
+				{#if error_message}
+					<button on:click={() => copy()}>Copy error code</button>
+				{:else}
+					<button class="hidden" />
+				{/if}
+			</div>
+		</div>
 	{/if}
 </main>
 
@@ -81,26 +151,47 @@
 		display: flex;
 		flex-direction: column;
 		align-items: center;
-		justify-content: center;
-		margin-top: 80px;
 		height: 100vh;
-		padding: 0 5rem;
+		padding: 2rem 4rem;
+
+		.header {
+			text-align: center;
+
+			h3 {
+				opacity: 0.5;
+				font-weight: 500;
+				font-size: 1rem;
+			}
+		}
+
+		.content {
+			flex-grow: 1;
+			margin-top: 4rem;
+		}
+
+		.footer {
+		}
 	}
 
-	h2 {
-		text-align: center;
-		margin-top: 2rem;
-		font-size: 1.25rem;
-		font-weight: 600;
-		color: rgba(255, 255, 255, 0.9);
-	}
+	.title {
+		display: flex;
+		flex-direction: column;
+		gap: 1rem;
 
-	h3 {
-		text-align: center;
-		margin-top: 0;
-		font-size: 1rem;
-		font-weight: 500;
-		color: rgba(255, 255, 255, 0.85);
+		h1,
+		h2,
+		h3 {
+			margin: 0;
+		}
+
+		h1 {
+			font-size: 2rem;
+		}
+
+		h2 {
+			font-size: 1.5rem;
+			opacity: 0.5;
+		}
 	}
 
 	.progress {
@@ -141,15 +232,38 @@
 		}
 	}
 
+	.buttons {
+		display: flex;
+		flex-grow: 1;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.pill,
 	button {
-		font-size: 0.8em;
+		font-size: 0.9em;
 		opacity: 0.85;
-		cursor: pointer;
+		margin: 0 0.2rem;
+
 		background-color: rgba(255, 255, 255, 0.1);
 		border-radius: 10px;
 		border: 2px solid rgba(255, 255, 255, 0.25);
-		padding: 5px 10px;
+		padding: 5px 12px;
 		transition: 200ms ease;
+	}
+
+	button {
+		cursor: pointer;
+
+		&.main {
+			background-color: $accent;
+			border: 2px solid rgba(255, 255, 255, 0.5);
+			font-weight: bold;
+
+			&:hover {
+				background-color: lighten($accent, 0.6);
+			}
+		}
 
 		&:hover {
 			background-color: rgba(255, 255, 255, 0.25);
@@ -158,6 +272,12 @@
 		&:active {
 			transform: scale(0.95);
 		}
+	}
+
+	a {
+		color: $accent;
+		cursor: pointer;
+		text-decoration: underline;
 	}
 
 	@keyframes move {
